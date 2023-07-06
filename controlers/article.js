954dc149e924e41b89313d72ca8e4da266a96bca
AlvaroGReg <alvaro.gracia.reg@gmail.com>
2023-07-06 13:11:27 +0200

    fixed new-article bug

--- controlers/article.js
+++ controlers/article.js
@@ -1,119 +1,105 @@
-
 const validator = require("validator")
 const Article = require("../models/Article")
 const { query } = require("express")
 
 const test = (req, res) => {
-
-    return res.status(200).json({
-        msg: "Acción de prueba en el controlador"
-    }
-    )
-}
+	return res.status(200).json({
+		msg: "Acción de prueba en el controlador",
+	})
+};
 
 const course = (req, res) => {
+	console.log("Se ha ejecutado el endpoint 'probando'")
 
-    console.log("Se ha ejecutado el endpoint 'probando'")
-
-    return res.status(200).json([{
-        curso: "Master en React",
-        autor: "Alvaro GR",
-        url: "alvarogreg.glitch.me"
-    },{
-        curso: "Master en React",
-        autor: "Alvaro GR",
-        url: "alvarogreg.glitch.me"
-    }
-])
-}
+	return res.status(200).json([
+		{
+			curso: "Master en React",
+			autor: "Alvaro GR",
+			url: "alvarogreg.glitch.me",
+		},
+		{
+			curso: "Master en React",
+			autor: "Alvaro GR",
+			url: "alvarogreg.glitch.me",
+		},
+	])
+};
 
 const create = (req, res) => {
+	//Recoger parámetros por post a guardar
+	let params = req.body
 
-    //Recoger parámetros por post a guardar
-    let params = req.body
+	//Validar datos
+	try {
+		let validate_title =
+			!validator.isEmpty(params.title) &&
+			validator.isLength(params.title, { min: 3, max: undefined })
+		let validate_content = !validator.isEmpty(params.content)
 
-    //Validar datos
-    try{
+		if (!validate_title || !validate_content) {
+			throw new Error("No se ha validado la info.")
+		}
+	} catch (error) {
+		return res.status(400).json({
+			status: "error",
+			msg: "Faltan datos por enviar",
+		})
+	}
 
-        let validate_title = !validator.isEmpty(params.title) &&
-                            validator.isLength(params.title, {min: 3, max: undefined})
-        let validate_content = !validator.isEmpty(params.content)
+	//Crear objeto a guardar y asignar valores
 
-        if(!validate_title || !validate_content){
-            throw new Error("No se ha validado la info.")
-        }
+	const newArticle = new Article(params)
 
-    }catch(error){
+	//Asignar valores de manera manual:
+	//MANUAL : newArticle.title = params.title
 
-        return res.status(400).json({
-            status: "error",
-            msg: "Faltan datos por enviar"
-        })
-    }
-
-    //Crear objeto a guardar y asignar valores
-
-    const newArticle = new Articulo(params)
-
-    //Asignar valores de manera manual:
-    //MANUAL : newArticle.title = params.title
-
-    //Guardar el artículo en la db
-    try {
-        
-        newArticle.save()
-        return res.status(200).json({
-            status: "success",
-            article: newArticle,
-            msg: "Artículo guardado con éxito."
-        })
-    } catch (error) {
-
-        return res.status(400).json({
-            status: "error",
-            msg: "No se ha guardado el artículo."
-        })
-    }
+	//Guardar el artículo en la db
+	try {
+		newArticle.save();
+		return res.status(200).json({
+			status: "success",
+			article: newArticle,
+			msg: "Artículo guardado con éxito.",
+		});
+	} catch (error) {
+		return res.status(400).json({
+			status: "error",
+			msg: "No se ha guardado el artículo.",
+		})
+	}
 }
 
 const list = async (req, res) => {
-
 	let num = 20
-	if(req.params.lasts){
-
+	if (req.params.lasts) {
 		num = 3
 	}
 
-    try {
+	try {
+		const query = await Article.find({}).sort({ date: -1 }).limit(num).exec()
 
-		const query = await Article.find({}).sort({date: -1}).limit(num).exec()
-	
-      
-        if (!query || query.length === 0) {
-          return res.status(404).json({
-            status: "error",
-            msg: "No se han encontrado artículos."
-          })
-        }
-      
-        return res.status(200).send({
-          status: "succes",
-          articles: query
-        })
-      } catch (error) {
-		console.log(error)
-        return res.status(500).json({
-			
-          status: "error",
-          msg: "Se produjo un error en la consulta de artículos."
-        })
-      }
-      
+		if (!query || query.length === 0) {
+			return res.status(404).json({
+				status: "error",
+				msg: "No se han encontrado artículos.",
+			});
+		}
+
+		return res.status(200).send({
+			status: "succes",
+			articles: query,
+		})
+	} catch (error) {
+		return res.status(500).json({
+			status: "error",
+			msg: "Se produjo un error en la consulta de artículos.",
+		})
+	}
 }
 
 module.exports = {
-    test,
-    course,
-    create,
-    list
+	test,
+	course,
+	create,
+	list,
 }
